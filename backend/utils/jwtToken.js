0df0ebdb3e105a34ea1dbc880082d507febcb796
Nilesh Dhankar <dhankarnilesh2006@gmail.com>
2026-04-12 15:29:59 +0530

    fixed cookie option for production

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,9 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: "lax",
-    secure: false ,
-    // Set httpOnly to true
+    sameSite: "none",
+    secure: true ,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
